d2fdeb6eed6887f5a74b0c4a321923fdc32f15d2
ng-23 <23grattan@gmail.com>
2024-10-29 22:39:15 -0400

    Initial commit
    First commit

---
+++ src/studious_spoon/feature_extraction.py
@@ -0,0 +1,321 @@
+import argparse
+import os
+import json
+import utils
+import torchvision
+import torch
+import h5py
+import numpy as np
+from tqdm import tqdm
+from sklearn.utils import shuffle
+from torchvision.models.feature_extraction import create_feature_extractor
+
+def get_args_parser():
+    parser = argparse.ArgumentParser(
+        description='-= PyTorch DCNN feature extraction script -=', 
+        add_help=True,
+        )
+    
+    parser.add_argument(
+        'dataset_path',
+        metavar='dataset-path',
+        type=str,
+        help='Filepath to image dataset, structured like ImageNet',
+        )
+    
+    parser.add_argument(
+        'model', 
+        type=str, 
+        choices=list(FeatureExtractor.supported_models), 
+        help='DCNN model to load weights into and extract features from',
+        )
+    
+    parser.add_argument(
+        'checkpoint_path', 
+        metavar='checkpoint-path', 
+        type=str, 
+        help='Filepath to model checkpoint to load weights from',
+        )
+    
+    parser.add_argument(
+        'layer', 
+        type=str, 
+        help='Layer of model to extract features from',
+        )
+    
+    parser.add_argument(
+        '--transforms-config', 
+        type=str, 
+        default=None, 
+        help='Path to JSON configuration file for dataset transforms',
+        )
+    
+    parser.add_argument(
+        '--batch-size', 
+        type=int, 
+        default=64, 
+        help='Dataloader batch size',
+        )
+    
+    parser.add_argument(
+        '--num-workers', 
+        type=int, 
+        default=0, 
+        help='Number of worker processes to spawn when loading data',
+        )
+    
+    parser.add_argument(
+        '--device', 
+        type=str, 
+        default='cuda', 
+        help='Hardware device to use',
+        )
+    
+    parser.add_argument(
+        '--flatten-features', 
+        action='store_true', 
+        help='If specified, flatten feature vectors before saving',
+        )
+    
+    parser.add_argument(
+        '--seed', 
+        type=int, 
+        default=42, 
+        help='Seed to use for controlling randomness',
+        )
+    
+    parser.add_argument(
+        '--dataset-name', 
+        type=str, 
+        choices=['train','val','test'], 
+        default='train', 
+        help='Name of dataset when saving',
+        )
+    
+    parser.add_argument(
+        '--output-dir', 
+        type=str, 
+        default=None, 
+        help='Filepath of directory to save features dataset to',
+        )
+    
+    return parser
+
+class FeatureExtractor:
+    '''
+    Feature extractor for DCNNs
+
+    DCNN architectures:
+    - AlexNet, VGG, GoogLeNet - https://www.digitalocean.com/community/tutorials/popular-deep-learning-architectures-alexnet-vgg-googlenet
+    - ResNet - https://arxiv.org/pdf/1512.03385, https://towardsdatascience.com/understanding-and-visualizing-resnets-442284831be8
+    - GoogLeNet/InceptionV1 - https://arxiv.org/pdf/1409.4842, https://viso.ai/deep-learning/googlenet-explained-the-inception-model-that-won-imagenet/
+    - EfficientNet - https://towardsdatascience.com/complete-architectural-details-of-all-efficientnet-models-5fd5b736142
+    '''
+
+    supported_models = {'alexnet','vgg16','vgg19','resnet18','googlenet',}
+
+    alexnet_nodes = {
+        'conv1':'features.0', 
+        'conv2':'features.3', 
+        'conv3':'features.6',
+        'conv4':'features.8',
+        'conv5':'features.10',
+
+        'fc7':'classifier.1',
+        'fc8':'classifier.4'
+        }
+    
+    vgg16_nodes = {
+        'conv1-1':'features.0',
+        'conv1-2':'features.2',
+
+        'conv2-1':'features.5',
+        'conv2-1':'features.7',
+
+        'conv3-1':'features.10',
+        'conv3-2':'features.12',
+        'conv3-3':'features.14',
+
+        'conv4-1':'features.17',
+        'conv4-2':'features.19',
+        'conv4-3':'features.21',
+
+        'conv5-1':'features.24',
+        'conv5-2':'features.26',
+        'conv5-3':'features.28',
+
+        'fc6':'classifier.0',
+        'fc7':'classifier.3',
+        }
+    
+    vgg19_nodes = {
+        'conv1-1':'features.0',
+        'conv1-2':'features.2',
+
+        'conv2-1':'features.5',
+        'conv2-2':'features.7',
+
+        'conv3-1':'features.10',
+        'conv3-2':'features.12',
+        'conv3-3':'features.14',
+        'conv3-4':'features.16',
+        
+        'conv4-1':'features.19',
+        'conv4-2':'features.21',
+        'conv4-3':'features.23',
+        'conv4-5':'features.25',
+        
+        'conv5-1':'features.28',
+        'conv5-2':'features.30',
+        'conv5-3':'features.32',
+        'conv5-4':'features.34',
+
+        'fc6':'classifier.0',
+        'fc7':'classifier.3',
+        }
+    
+    resnet18_nodes = {
+        'conv2-1':'layer1.0.conv1',
+        'conv2-2':'layer1.0.conv2',
+        'conv2-3':'layer1.1.conv1',
+        'conv2-4':'layer1.1.conv2',
+
+        'conv3-1':'layer2.0.conv1',
+        'conv3-2':'layer2.0.conv2',
+        'conv3-3':'layer2.1.conv1',
+        'conv3-4':'layer2.1.conv2',
+
+        'conv4-1':'layer3.0.conv1',
+        'conv4-2':'layer3.0.conv2',
+        'conv4-3':'layer3.1.conv1',
+        'conv4-4':'layer3.1.conv2',
+
+        'conv5-1':'layer4.0.conv1',
+        'conv5-2':'layer4.0.conv2',
+        'conv5-3':'layer4.1.conv1',
+        'conv5-4':'layer4.1.conv2',
+        }
+    
+    googlenet_nodes = {
+        'conv1':'conv1.conv',
+        'conv2':'conv2.conv',
+        'conv3':'conv3.conv',
+        'inception3a':'inception3a.cat',
+        'inception3b':'inception3b.cat',
+        'inception4a':'inception4a.cat',
+        'inception4b':'inception4b.cat',
+        'inception4c':'inception4c.cat',
+        'inception4d':'inception4d.cat',
+        'inception4e':'inception4e.cat',
+        'inception5a':'inception5a.cat',
+        'inception5b':'inception5b.cat',
+        }
+    
+    def __init__(self, model, model_name, layer):
+        self.model = model
+        self.model_name = model_name
+        self.layer = layer
+
+    def _get_nodes_dict(self, model_name:str) -> dict:
+        if not hasattr(FeatureExtractor, f'{model_name}_nodes'):
+            raise Exception(f'No such nodes dict for model {model_name}')
+        
+        return getattr(FeatureExtractor, f'{model_name}_nodes')
+
+    def extract_features(self, dataloader, device, flatten=False):
+        model_nodes = self._get_nodes_dict(self.model_name)
+
+        if self.layer not in model_nodes:
+            raise Exception(f'{self.model_name} has no node(s) for layer {self.layer}. Valid layers are {list(model_nodes.keys())}')
+        
+        node = model_nodes[self.layer]
+        
+        fex_model = create_feature_extractor(self.model, return_nodes=[node])
+
+        # maps an encoded class name to its string name
+        # ex: {0:'car', 1:'boat'}
+        encoded_labels_mapping = {v: k for k, v in dataloader.dataset.class_to_idx.items()}
+
+        # features[i] is the feature vector for image class instance labels[i]
+        img_features, img_labels = [], []
+
+        with torch.inference_mode():
+            for i, (samples, targets) in enumerate(tqdm(dataloader, desc='Processing batches...')):
+                samples, targets = samples.to(device), targets.to(device)
+
+                # output is a dict mapping a node (eg fc)
+                # to a tensor of tensors representing the features for each image in the batch
+                output = fex_model(samples)
+
+                features = output[node]
+
+                for j in range(len(features)):
+                    img_features.append(features[j].cpu().numpy())
+                    img_labels.append(encoded_labels_mapping[targets[j].cpu().item()])
+
+                    if flatten:
+                        img_features[-1] = img_features[-1].flatten()
+
+        return np.array(img_features), np.array(img_labels)
+
+def save_as_dataset(features, labels, dataset_name, filename='', output_dir=''):
+    if not filename:
+        filename = 'dataset_cache.h5'
+    
+    output_path = filename
+    if output_dir:
+        output_path = os.path.join(output_dir, filename)
+
+    with h5py.File(output_path, 'w') as h5_file:
+        # save image features
+        h5_file.create_dataset(f'imgs/{dataset_name}', data=features)
+        
+        # save labels as strings in HDF5 (strings need to be encoded to bytes)
+        h5_file.create_dataset(f'labels/{dataset_name}', data=utils.encode_str_list(labels))
+    
+def main(args:argparse.Namespace):
+    # save command line args to disk
+    cmd_args_output_path = 'cmd_args.json'
+    if args.output_dir is not None:
+        os.makedirs(args.output_dir, exist_ok=True)
+        cmd_args_output_path = os.path.join(args.output_dir, cmd_args_output_path)
+    with open(cmd_args_output_path, 'w') as f:
+        json.dump(vars(args), f, indent=4)
+
+    print('Making dataset transforms...')
+    dataset_transforms = utils.make_dataset_transforms(None if args.transforms_config is None else json.load(open(args.transforms_config)))
+
+    print('Loading dataset...')
+    dataset = utils.load_dataset(args.dataset_path)
+    dataset.transform = dataset_transforms
+    num_classes = len(dataset.classes)
+    print(f'Dataset has {len(dataset)} samples of {num_classes} classes')
+    
+    print('Making dataloaders...')
+    dl_config = utils.gen_standard_dataloader_config()
+    dl_config['batch_size'] = args.batch_size; dl_config['num_workers'] = args.num_workers
+    dataloader = utils.make_dataloader(dataset, dataloader_config=dl_config)
+
+    print(f'Creating {args.model} model...')
+    device = torch.device(args.device)
+    model = torchvision.models.get_model(args.model, num_classes=num_classes)
+    model_checkpoint = torch.load(args.checkpoint_path)
+    model.load_state_dict(model_checkpoint['model'])
+    model.to(device)
+
+    print('Extracting features...')
+    fex = FeatureExtractor(model, args.model, args.layer)
+    features, labels = fex.extract_features(dataloader, device, flatten=args.flatten_features)
+
+    print('Randomly shuffling features and labels...')
+    features, labels = shuffle(features, labels, random_state=args.seed)
+
+    print('Saving features...')
+    save_as_dataset(features, labels, args.dataset_name, output_dir=args.output_dir)
+
+    print('Done!')
+
+if __name__ == '__main__':
+    parser = get_args_parser()
+    args = parser.parse_args()
+    main(args)
---
+++ src/studious_spoon/utils.py
@@ -0,0 +1,198 @@
+import os
+import torch
+import torch.utils
+import torchvision
+import numpy as np
+import torch.utils.data
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+from schemas import validate_config
+from torch.optim.swa_utils import AveragedModel, get_ema_multi_avg_fn
+from torcheval.metrics.functional import multiclass_accuracy, multiclass_recall, multiclass_f1_score, multiclass_precision 
+
+SUPPORTED_OPTIMIZERS = {
+    'adam':optim.Adam, 
+    'adamw':optim.AdamW, 
+    'sgd':optim.SGD, 
+    'rmsprop':optim.RMSprop,
+    }
+
+SUPPORTED_LR_SCHEDULERS = {
+    'step_lr':optim.lr_scheduler.StepLR, 
+    'exponential_lr':optim.lr_scheduler.ExponentialLR, 
+    'cosineannealing_lr':optim.lr_scheduler.CosineAnnealingLR,
+    }
+
+class EarlyStopper:
+    '''
+    See https://stackoverflow.com/a/76602544
+    '''
+
+    def __init__(self, patience=1, min_delta=0.0):
+        self.patience = patience  # number of times to allow for no improvement before stopping the execution
+        self.min_delta = min_delta  # the minimum change to be counted as improvement
+        self.counter = 0  # count the number of times the validation accuracy not improving
+        self.min_validation_loss = np.inf
+
+    # return True when validation loss is not decreased by the `min_delta` for `patience` times 
+    def early_stop(self, validation_loss):
+        if ((validation_loss+self.min_delta) < self.min_validation_loss):
+            self.min_validation_loss = validation_loss
+            self.counter = 0  # reset the counter if validation loss decreased at least by min_delta
+        
+        elif ((validation_loss+self.min_delta) >= self.min_validation_loss):
+            self.counter += 1 # increase the counter if validation loss is not decreased by the min_delta
+            if self.counter >= self.patience:
+                return True
+        
+        return False
+    
+def encode_str_list(str_list):
+    return [s.encode('utf-8') for s in str_list]
+    
+def gen_default_transforms_config():
+    return {
+        'Resize':{'size':(224,224),},
+        'ToTensor':{},
+        'Normalize':{'mean':[0.485, 0.456, 0.406], 'std':[0.229, 0.224, 0.225],},
+        }
+
+def make_transforms_pipeline(transforms_config:dict|None=None):
+    pipeline = []
+
+    if transforms_config is None:
+        transforms_config = gen_default_transforms_config()
+        
+    for transform_name in transforms_config:
+        if not hasattr(transforms, transform_name):
+            raise Exception(f'No such transform {transform_name}')
+        
+        transform_config = validate_config(transforms_config[transform_name], transform_name)
+
+        transform_func = getattr(transforms, transform_name)
+        pipeline.append(transform_func(**transform_config))
+            
+    return transforms.Compose(pipeline)
+
+def load_dataset(filepath:str):
+    return torchvision.datasets.ImageFolder(root=filepath)
+
+def split_dataset(dataset:torchvision.datasets.ImageFolder, train_ratio:float=0.6, val_ratio:float=0.2, test_ratio:float=0.2, seed=42):
+    if train_ratio + val_ratio + test_ratio > 1.0:
+        raise Exception('Train/val/test ratios must be <= 1.0')
+    
+    generator = torch.Generator().manual_seed(seed)
+    
+    return torch.utils.data.random_split(dataset, [train_ratio, val_ratio, test_ratio], generator=generator)
+
+def gen_default_dataloader_config():
+    return {
+        'batch_size':32,
+        'shuffle':False,
+        'num_workers':0,
+        'pin_memory':True,
+        }
+
+def make_dataloader(dataset, dataloader_config:dict|None=None):
+    if dataloader_config is None:
+        dataloader_config = gen_default_dataloader_config()
+
+    dataloader_config = validate_config(dataloader_config, 'DataLoader')
+
+    return torch.utils.data.DataLoader(dataset, **dataloader_config)
+
+def make_optimizer(model_params, optimizer:str, optimizer_config:dict|None=None):
+    optimizer = optimizer.lower()
+
+    if optimizer_config is None:
+        optimizer_config = {} # use optimizer's defaults
+
+    if optimizer not in SUPPORTED_OPTIMIZERS:
+        raise Exception(f'Unknown/unsupported optimizer {optimizer}. Supported ones are currently {list(SUPPORTED_OPTIMIZERS.keys())}')
+    optim_class = SUPPORTED_OPTIMIZERS[optimizer]
+
+    optimizer_config = validate_config(optimizer_config, optim_class.__name__)
+
+    return optim_class(model_params, **optimizer_config)
+
+def make_lr_scheduler(optimizer, lr_scheduler:str, lr_scheduler_config:dict|None=None):
+    lr_scheduler = lr_scheduler.lower()
+
+    if lr_scheduler_config is None:
+        lr_scheduler_config = {} # use learning rate scheduler's defaults
+
+    if lr_scheduler not in SUPPORTED_LR_SCHEDULERS:
+        raise Exception(f'Unknown/unsupported learning rate scheduler {lr_scheduler}. Supported learning rate schedulers are {list(SUPPORTED_LR_SCHEDULERS.keys())}')
+    lr_scheduler_class = SUPPORTED_LR_SCHEDULERS[lr_scheduler]
+
+    lr_scheduler_config = validate_config(lr_scheduler_config, lr_scheduler_class.__name__)
+
+    return lr_scheduler_class(optimizer, **lr_scheduler_config)
+
+def make_model_ema(model, model_ema_config:dict):
+    model_ema_config = validate_config(model_ema_config, 'ModelEMA')
+
+    return AveragedModel(
+        model, 
+        multi_avg_fn=get_ema_multi_avg_fn(decay=model_ema_config['decay']),
+        )
+
+def make_early_stopper(early_stopper_config:dict|None=None):
+    if early_stopper_config is None:
+        early_stopper_config = {}
+
+    early_stopper_config = validate_config(early_stopper_config, 'EarlyStopper')
+
+    return EarlyStopper(**early_stopper_config)
+
+def calc_metrics(preds, targets, average='micro', num_classes:int|None=None, ):
+    metrics = {}
+
+    acc = multiclass_accuracy(preds, targets, average=average, num_classes=num_classes).item()
+    recall = multiclass_recall(preds, targets, average=average, num_classes=num_classes).item()
+    precision = multiclass_precision(preds, targets, average=average, num_classes=num_classes).item()
+    f1 = multiclass_f1_score(preds, targets, average=average, num_classes=num_classes).item()
+
+    metrics['acc'] = acc
+    metrics['recall'] = recall
+    metrics['precision'] = precision
+    metrics['f1'] = f1
+
+    return metrics
+
+def plot_comparison(x, y1, y2, label1:str='A', label2:str='B', title:str='', x_label:str='', y_label:str='', filename='plot.png', output_dir=''):
+    plt.plot(x, y1, label=label1)
+    plt.plot(x, y2, label=label2)
+
+    if title:
+        plt.title(title)
+    if x_label:
+        plt.xlabel(x_label)
+    if y_label:
+        plt.ylabel(y_label)
+    plt.legend()
+
+    output_path = filename
+    if output_dir:
+        output_path = os.path.join(output_dir, output_path)
+    plt.savefig(output_path)
+    plt.close()
+
+def save_checkpoint(model, optimizer, epoch, model_ema=None, lr_scheduler=None, output_dir=''):
+    checkpoint = {
+        'model':model.state_dict(),
+        'optimizer':optimizer.state_dict(),
+        'epoch':epoch,
+    }
+    
+    if lr_scheduler is not None:
+        checkpoint['lr_scheduler'] = lr_scheduler.state_dict()
+
+    if model_ema is not None:
+        checkpoint['model_ema'] = model_ema.state_dict()
+
+    torch.save(
+        checkpoint, 
+        os.path.join(output_dir, f'checkpoint_{epoch}.pth'),
+        )
